bdd8cb07a3de1fc40cdb8c54bee668d1106afa27
Rafi Priatna K <rafipriatnak@gmail.com>
2021-07-28 20:03:34 +0700

    Fix typo

--- app/Http/Requests/Admin/ProductRequest.php
+++ app/Http/Requests/Admin/ProductRequest.php
@@ -25,7 +25,7 @@ class ProductRequest extends FormRequest
     {
         return [
             'name' => 'required|max:255',
-            'price' => 'required|integer',
+            'prices' => 'required|integer',
             'description' => 'required',
         ];
     }
